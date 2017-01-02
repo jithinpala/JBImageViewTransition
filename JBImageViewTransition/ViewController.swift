d4c0209a6ce76f1c9805c8689b7cc490a2050c74
Jithin B <jithin.b@mac114.local>
2017-01-02 14:50:04 +0530

    First commit

--- JBImageViewTransition/ViewController.swift
+++ JBImageViewTransition/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageTransitionView: JBTransitionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //***** Configure image transition animation *****//
+        imageTransitionView.setAnimationImages(NSArray(objects: "image1.jpg","image2.jpg","image3.jpg","image4.jpg"))
+        imageTransitionView.animationFromAllSide = true
+        imageTransitionView.animationDuration = 3
+        imageTransitionView.startJNImageAnimation()
+    }
+    
+    
+    @IBAction func stopAnimationButtonTapAction(sender: AnyObject) {
+        imageTransitionView.stopJNImageAnimation()
     }
 
     override func didReceiveMemoryWarning() {
